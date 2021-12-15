30fd0869ad1fa0b6f26deb540f58f681f02b920e
Young <81794430+Daeyeong-hub@users.noreply.github.com>
2021-12-15 15:49:01 +0900

    Fix : Router연결 Routes 컴포넌트로 묶어줄 컴포넌트를 리액트에서 기본적으로 제공하는 App.js로 사용했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 import { Routes, Route } from 'react-router-dom';
+import LoginYoonkyeong from './pages/yoonkyeong/Login/Login';
+import MainYoonkyeong from './pages/yoonkyeong/Main/Main';
+import LoginDaeyeong from './pages/daeyeong/Login/Login';
+import MainDaeyeong from './pages/daeyeong/Main/Main';
+import LoginJoonyoung from './pages/joonyoung/Login/Login';
+import MainJoonyoung from './pages/joonyoung/Main/Main';
+import LoginHyeonze from './pages/hyeonze/Login/Login';
+import MainHyeonze from './pages/hyeonze/Main/Main';
 import Navbar from './components/Navbar';
+import './styles/common.scss';
+import './styles/reset.scss';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar />
-      {/* <h1>Hello world</h1> */}
-      {/* 윤경님 */}
-      {/* <Route path='/login-yoonkyeong' element={}/>  */}
-      {/* <Route path="/main-yoonkyeong" element={}/> */}
-      {/* 대영님 */}
-      {/* <Route path='/login-daeyeong'element={} /> */}
-      {/* <Route path='/main-daeyeong' element={}/> */}
+      <Routes>
+        {/* 윤경님 */}
+        <Route path="/login-yoonkyeong" element={<LoginYoonkyeong />} />
+        <Route path="/main-yoonkyeong" element={<MainYoonkyeong />} />
 
-      {/* 현재님 */}
-      {/* <Route path='/login-hyeonze' element={} /> */}
-      {/* <Route path='/main-hyeonze' element={} /> */}
+        {/* 대영님 */}
+        <Route path="/login-daeyeong" element={<LoginDaeyeong />} />
+        <Route path="/main-daeyeong" element={<MainDaeyeong />} />
 
-      {/* 준영님 */}
-      {/* <Route path='/login-joonyoung' element={} /> */}
-      {/* <Route path="/main-joonyoung" element={} /> */}
-    </>
+        {/* 현재님 */}
+        <Route path="/login-hyeonze" element={<LoginHyeonze />} />
+        <Route path="/main-hyeonze" element={<MainHyeonze />} />
+
+        {/* 준영님 */}
+        <Route path="/login-joonyoung" element={<LoginJoonyoung />} />
+        <Route path="/main-joonyoung" element={<MainJoonyoung />} />
+      </Routes>
+    </div>
   );
 }
 
