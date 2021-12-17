e2b43d87fbf7aebc5f55f3113fe34aa165698c08
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-12-17 09:55:06 +0900

    [ADD] 피드의 아이콘 영역 추가

--- src/pages/joonyoung/Main/Feed/Feed.jsx
+++ src/pages/joonyoung/Main/Feed/Feed.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FeedHeader from './FeedHeader';
 import FeedCarousel from './FeedCarousel';
 import './Feed.scss';
+import FeedIcons from './FeedIcons';
 
 const Feed = () => {
+  const [likes, setLikes] = useState(1129);
+
   return (
     <section className="feed">
       <FeedHeader />
       <FeedCarousel />
-      <h2>Hello world</h2>
+      <FeedIcons likes={likes} setLikes={setLikes} />
       {/* Feed icon */}
       {/* feed Comment */}
     </section>
---
+++ src/pages/joonyoung/Main/Feed/FeedComment/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FeedComment = () => {
+  return <div />;
+};
+
+export default FeedComment;
