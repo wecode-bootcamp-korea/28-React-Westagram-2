8cc0413fa28099743442b1fcd5629ca2e4010c82
YOONKY <lov012726@gmail.com>
2021-12-17 15:36:30 +0900

    ADD : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/yoonkyeong/Login/Login.jsx
+++ src/pages/yoonkyeong/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
@@ -6,6 +6,17 @@ export default function LoginYoonkyeong() {
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/main-yoonkyeong');
+  };
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const handleIdInput = e => {
+    setId(e.target.value);
+    // console.log(id);
+    // 여기서 콘솔을 찍어서 value가 아이디값으로 들어오는 걸 보고싶었는데, 한줄 늦게 콘솔이 찍힙니다.
+  };
+  const handlePasswordInput = e => {
+    setPassword(e.target.value);
   };
   return (
     <div className="login">
@@ -16,13 +27,13 @@ export default function LoginYoonkyeong() {
         <input
           type="text"
           id="idInput"
-          name="id"
+          onChange={handleIdInput}
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
         <input
           type="password"
           id="passwordInput"
-          name="password"
+          onChange={handlePasswordInput}
           placeholder="비밀번호"
         />
         <input type="button" id="btnLogin" onClick={goToMain} value="로그인" />
