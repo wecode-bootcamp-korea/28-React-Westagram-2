5b23300731103922657a19cb0ba38d7563a5edef
hyunjea Ann <hyunjeaann@hyunjeaui-MacBookAir.local>
2021-12-21 21:42:47 +0900

    Fix : 미사용 데이터 삭제

--- src/pages/hyeonze/Main/Main.jsx
+++ src/pages/hyeonze/Main/Main.jsx
@@ -6,7 +6,6 @@ import './Main.scss';
 const Main = () => {
   const [feedVal, setfeedVal] = useState([]);
   const currInitialComment = [];
-  const [initialComment, setinitialComment] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/data/feedData.json', {
@@ -15,12 +14,9 @@ const Main = () => {
       res.json().then(data => {
         setfeedVal(data);
         for (let i in data) currInitialComment.push(data[i].comment);
-        setinitialComment(currInitialComment);
       })
     );
   }, []);
-  console.log(initialComment);
-  // console.log(initialComment[0][0].id);
 
   return (
     <div className="main">
